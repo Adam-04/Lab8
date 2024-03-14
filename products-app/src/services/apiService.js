f497cbdf909b51f7703431bd74ba2dfef63c4300
Adam-04 <144634921+Adam-04@users.noreply.github.com>
2024-03-13 19:05:16 -0600

    Step C

--- products-app/src/services/apiService.js
+++ products-app/src/services/apiService.js
@@ -1,34 +1,41 @@
-import axios from 'axios';
-const BASE_URL = 'https://dummyjson.com/products';
+import axios from "axios";
+const BASE_URL = "https://dummyjson.com/products";
 
 export const fetchProducts = async () => {
-    try {
-        const response = await axios.get(BASE_URL);
-        return response.data;
-    } catch(error){
-        throw error; // throw error to the caller
-    }
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (error) {
+    throw error; // throw error to the caller
+  }
 };
 
 export const getProductDetails = async (id) => {
-    // implenment same logic as fetchProducts
+  // implenment same logic as fetchProducts
+  try {
+    const response = await axios.get(`${BASE_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error; // throw error to the caller
+  }
 };
 
 export const removeProduct = async (id) => {
-    try {
-        const response = await axios.delete(`${BASE_URL}/${id}`);
-        return response.data;
-    } catch(error){
-        throw error; // throw error to the caller
-    }
+  try {
+    const response = await axios.delete(`${BASE_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error; // throw error to the caller
+  }
 };
 
 // Adds new product to the database
 export const addProduct = (product) => {
-    return axios.post(`${BASE_URL}/add`, JSON.stringify(product));
+  return axios.post(`${BASE_URL}/add`, JSON.stringify(product));
 };
 
 // Edits existing product by ID
 export const editProduct = (id, product) => {
-    // implement it using similar logic as addProduct function but use axios.put method
+  // implement it using similar logic as addProduct function but use axios.put method
+  return axios.put(`${BASE_URL}/${id}/edit`, JSON.stringify(product));
 };
